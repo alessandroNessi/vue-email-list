910bbdb09c4983ed028adb03a4c45c4d85b68fb0
alessandroNessi <alessandronessi@msn.com>
2021-09-27 17:54:05 +0200

    esercizio e bonus completati

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,40 @@ var app = new Vue({
     el: "#root",
     data: {
         usersMails:[],
-        userId :0
+        userId :0,
+        n:"",
+        onChange:false
     },
     methods:{
-        addUser(n){
-            if(n>0){
+        addUser(){
+            this.n=parseInt(this.n);
+            if(this.n>0){
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((response)=>{
                     if(response.data.success){
                         this.usersMails.push({'id':this.userId,'email': response.data.response});
                         this.userId++;
-                        alert(this.userId);
-                        this.addUser(n-1);
+                        this.n--;
+                        this.addUser();
                     }
                 });
             }
+        },
+        deleteUser(index){
+            this.usersMails.splice(index,1);
+        },
+        startChange(index){
+            this.onChange=index;
+        },
+        change(){
+            let changeValue=document.getElementById("changeValue");
+            if(changeValue.value!=""&&changeValue.value.includes("@")&&changeValue.value.includes(".")){
+                this.usersMails[this.onChange].email=changeValue.value;
+                changeValue.value="";
+            }else{
+                alert("mail not valid");
+            }
+            this.onChange=false;
         }
-    },
-    mounted:
-        function(){
-                this.addUser(10);
-        }
+    }
 });
